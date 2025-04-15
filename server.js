ae110353becb04b0470b7481dcef6a91b107efba
SujalD04 <sujalrdixit@gmail.com>
2025-04-15 15:29:51 +0530

    Env variables

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const app = require('./src/app');
 
 const PORT = process.env.PORT || 3000;
+const ENV = process.env.NODE_ENV || 'development';
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Environment: ${ENV}`);
 });
-
-
